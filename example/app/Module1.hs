83e96ac7b0b44a7ff96f7ba33d956969a17a67b4
Adithya Kumar <adithya@composewell.com>
2024-06-07 03:01:23 +0530

    Modify the example app according to the generate format

--- example/app/Module1.hs
+++ example/app/Module1.hs
@@ -2,13 +2,10 @@ module Module1
     ( printFromModule1
     ) where
 
-import Simple.RPC.Server
-
-raw_printFromModule1 :: Int -> String -> Bool -> IO (Int, String, Bool)
-raw_printFromModule1 val1 val2 val3 = do
+printFromModule1 :: Int -> String -> Bool -> IO (Int, String, Bool)
+printFromModule1 val1 val2 val3 = do
     putStrLn "From Module1,"
     print val1
     putStrLn val2
     print val3
     pure (val1, val2, val3)
-$(rpcExport "raw_printFromModule1" "printFromModule1")
