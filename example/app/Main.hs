d04481e6f4d686937dee4db7633f3789f38f2c28
Adithya Kumar <adithya@composewell.com>
2024-06-13 02:36:13 +0530

    Generate the rpc server executable

---
+++ example/app/Main.hs
@@ -0,0 +1,27 @@
+-- Warning: This file is generated via CLI. Do not edit this directly.
+
+module Main (main) where
+
+--------------------------------------------------------------------------------
+-- Imports
+--------------------------------------------------------------------------------
+
+import Simple.RPC.Server
+import qualified Module4 as Module0
+import qualified Module3 as Module1
+import qualified Module2 as Module2
+import qualified Module1 as Module3
+
+--------------------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main =
+    mainWith "0.0.0"
+        [ evaluator Module0.top
+        , evaluator Module1.helloWorld
+        , evaluator Module1.bottom
+        , evaluator Module2.printFromModule2
+        , evaluator Module3.printFromModule1
+        ]
