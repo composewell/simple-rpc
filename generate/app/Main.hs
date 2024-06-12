053b32abd81ee84a0b773ae46df6c5fdcd1476c5
Adithya Kumar <adithya@composewell.com>
2024-06-13 02:34:52 +0530

    Update the generate exeutable

--- generate/app/Main.hs
+++ generate/app/Main.hs
@@ -7,13 +7,16 @@ module Main (main) where
 import Data.Function ((&))
 import Data.Maybe (fromMaybe)
 import Streamly.Unicode.String (str)
-import Data.Foldable (forM_)
-import System.FilePath ((</>), (<.>), takeDirectory)
+import System.FilePath ((</>))
 import Data.Functor.Identity (runIdentity)
 
-import qualified Streamly.Data.Stream as Stream
+import qualified Streamly.Internal.Data.Parser as Parser
+import qualified Streamly.Internal.Data.Stream as Stream
+import qualified Streamly.FileSystem.File as File
 import qualified Streamly.Data.Fold as Fold
 import qualified Streamly.Internal.System.Command as Cmd
+import qualified Streamly.Unicode.Stream as Unicode
+
 
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -27,44 +30,44 @@ import System.Environment (getArgs)
 import qualified Data.Aeson.KeyMap as KM
 import qualified Data.Aeson.Key as KM
 
+import qualified Streamly.Coreutils.Ls as Ls
+import Streamly.Coreutils.Common (Switch(..))
+
 --------------------------------------------------------------------------------
 -- Types
 --------------------------------------------------------------------------------
+
+data Config =
+    Config
+        { distBuildDir :: String
+        , sourceDirectories :: [String]
+        , cabalFile :: String
+        , libraryName :: String
+        , serverDirectory :: String
+        , serverExecutableName :: String
+        , serverConfigImportList :: [String]
+        , serverVersion :: String
+        }
 
 data RpcModule =
     RpcModule
         { moduleName :: String
         , functionList :: [String]
         }
-
-data Config =
-    Config
-        { cabalFile :: String
-        , libraryRelativeToCabal :: String
-        , libraryName :: String
-        , rpcModulePrefix :: String
-        , serverDirectoryRelativeToCabal :: String
-        , serverExecutableName :: String
-        , serverConfigImportList :: [String]
-        , serverVersion :: String
-        , rpcModuleList :: [RpcModule]
-        }
+    deriving (Show)
 
 parseConfigFromValue :: Value -> Config
 parseConfigFromValue (Object obj) =
     fromMaybe (error "Unable to parse the config file") $ do
         Config
-            <$> lookupText "cabalFile" obj
-            <*> lookupText "libraryRelativeToCabal" obj
-            <*> lookupText "libraryName" obj
-            <*> lookupText "rpcModulePrefix" obj
-            <*> lookupText "serverDirectoryRelativeToCabal" obj
-            <*> lookupText "serverExecutableName" obj
-            <*> lookupText "serverConfigImportList" obj
-            <*> lookupText "serverVersion" obj
-            <*> fmap
-                    parseRpcModuleList
-                    (KM.lookup (KM.fromString "rpcModuleList") obj)
+            <$> lookupVal "distBuildDir" obj
+            <*> lookupVal "sourceDirectories" obj
+            <*> lookupVal "cabalFile" obj
+            <*> lookupVal "libraryName" obj
+            <*> lookupVal "serverDirectory" obj
+            <*> lookupVal "serverExecutableName" obj
+            <*> lookupVal "serverConfigImportList" obj
+            <*> lookupVal "serverVersion" obj
 
     where
 
@@ -72,23 +75,61 @@ parseConfigFromValue (Object obj) =
         case fromJSON a of
             Error err -> error err
             Success val -> val
-    lookupText k v = fmap fromJSON' (KM.lookup (KM.fromString k) v)
-    parseRpcModuleList (Object rmlObj) =
-        map
-            (\(a, b) -> RpcModule (KM.toString a) (fromJSON' b))
-            (KM.toList rmlObj)
-    parseRpcModuleList _ = error "Expected an object for rpcModuleList"
+    lookupVal k v = fmap fromJSON' (KM.lookup (KM.fromString k) v)
 parseConfigFromValue _ = error "Expected an object"
+
+slashToDot :: String -> String
+slashToDot = map f
+    where
+    f '/' = '.'
+    f x = x
+
+--------------------------------------------------------------------------------
+-- Get module listing
+--------------------------------------------------------------------------------
+
+getRpcModuleList :: Config -> IO [RpcModule]
+getRpcModuleList Config{..} = do
+    Stream.fromList sourceDirectories
+        & fmap (\x -> distBuildDir </> x)
+        & Stream.concatMap exploreDir
+        & Stream.toList
+
+    where
+
+    exploreDir dir =
+        Ls.ls (Ls.recursive On) dir
+            & Stream.catRights
+            & Stream.mapM (parseTargetFile dir)
+
+parseTargetFile :: FilePath -> FilePath -> IO RpcModule
+parseTargetFile dirPrefix fp = do
+    fList <-
+        File.read fp
+            & Unicode.decodeUtf8'
+            & Stream.foldMany (Fold.takeEndBy_ (== '\n') Fold.toList)
+            & Stream.mapM (Stream.parse pragmaParser . Stream.fromList)
+            & Stream.catRights
+            & Stream.toList
+    pure $ RpcModule moduleName fList
+
+    where
+
+    moduleName =
+        let lenPrefix = length dirPrefix
+            val = drop (lenPrefix + 1) fp
+            lenVal = length val
+         in slashToDot $ take (lenVal - 6) val
+
+    -- {-# ANN functionName "RPC" #-}
+    pragmaParser =
+        Parser.listEq "{-# ANN "
+            *> Parser.fromFold (Fold.takeEndBy_ (== ' ') Fold.toList)
+            <* Parser.listEq "\"RPC\" #-}"
 
 --------------------------------------------------------------------------------
 -- Utils
 --------------------------------------------------------------------------------
-
-dotToSlash :: String -> String
-dotToSlash = map f
-    where
-    f '.' = '/'
-    f x = x
 
 docWarning :: Doc ann
 docWarning =
@@ -119,15 +160,18 @@ docSectionComment val =
 -- Generation
 --------------------------------------------------------------------------------
 
-generateServer :: Config -> Text
-generateServer Config{..} = do
-    renderStrict $ layoutPretty defaultLayoutOptions $ vsep
+generateServer :: Config -> IO Text
+generateServer conf@Config{..} = do
+    rpcModuleList <- getRpcModuleList conf
+    let docList = map getDoc $ zip ([0..] :: [Int]) rpcModuleList
+    pure
+        $ renderStrict $ layoutPretty defaultLayoutOptions $ vsep
         $ [docWarning, emptyDoc]
        ++ [docModuleDef]
        ++ docSectionComment "Imports"
-       ++ [docImportList]
+       ++ [docImportList docList]
        ++ docSectionComment "Main"
-       ++ docMainDef
+       ++ docMainDef docList
        ++ [emptyDoc]
 
 
@@ -136,18 +180,15 @@ generateServer Config{..} = do
     docModuleDef = pretty "module Main (main) where"
 
     getDoc (i, RpcModule{..}) =
-        let rpcModuleName = rpcModulePrefix ++ "." ++ moduleName
-            iStr = show i
+        let iStr = show i
             mkEvalDoc x = [str|evaluator Module#{iStr}.#{x}|]
-         in ( pretty [str|import qualified #{rpcModuleName} as Module#{iStr}|]
+         in ( pretty [str|import qualified #{moduleName} as Module#{iStr}|]
             , map mkEvalDoc functionList
             )
 
-    docList = map getDoc $ zip ([0..] :: [Int]) rpcModuleList
-
-    docImportList =
+    docImportList docList =
         vsep $ pretty "import Simple.RPC.Server" : map fst docList
-    docEvalList =
+    docEvalList docList =
         let evalList = concat $ map snd docList
             pre = "[ " : replicate (length evalList - 1) ", "
          in vsep
@@ -155,74 +196,14 @@ generateServer Config{..} = do
                 , pretty "]"
                 ]
 
-    docMainDef =
+    docMainDef docList =
         [ pretty "main :: IO ()"
         , pretty "main ="
         , indent 4 $ vsep
               [ pretty [str|mainWith "#{serverVersion}"|]
-              , indent 4 $ docEvalList
+              , indent 4 $ docEvalList docList
               ]
         ]
-
-
-generateModule :: Config -> RpcModule -> Text
-generateModule Config{..} RpcModule{..} = do
-    renderStrict $ layoutPretty defaultLayoutOptions $ vsep
-        $ [docWarning, emptyDoc]
-       ++ docModuleDef
-       ++ docSectionComment "Imports"
-       ++ docImportList
-       ++ docSectionComment "RPC exports"
-       ++ docBody
-       ++ [emptyDoc]
-
-    where
-
-    rpcModuleName = rpcModulePrefix ++ "." ++ moduleName
-
-    docExportList =
-        let len = length functionList
-            pre = "( " : replicate (len - 1) ", "
-         in map pretty $ zipWith (++) pre functionList
-
-    docModuleDef =
-        [ pretty [str|module #{rpcModuleName}|]
-        , indent 4 $ vsep docExportList
-        , indent 4 $ pretty ") where"
-        ]
-
-    docImportList =
-        [ pretty [str|import qualified #{moduleName} as Lib|]
-        , pretty "import Simple.RPC.Server"
-        ]
-
-    makeRpcExportStatement fname =
-        pretty [str|$(rpcExport "Lib.#{fname}" "#{fname}")|]
-
-    docBody = map makeRpcExportStatement functionList
-
-generateCabalClient :: Config -> Text
-generateCabalClient Config{..} =
-    renderStrict $ layoutPretty defaultLayoutOptions docCommonClient
-
-    where
-
-    mkRpcModuleName x = rpcModulePrefix ++ "." ++ x
-    rpcModuleListNames = map (mkRpcModuleName . moduleName) rpcModuleList
-    pre = "  " : replicate (length rpcModuleListNames) ", "
-
-    docRpcModuleList =
-        vsep $ map pretty $ zipWith (++) pre rpcModuleListNames
-
-    docCommonClient =
-        vsep
-            [ docWarningCabal "rpc-client"
-            , emptyDoc
-            , indent 4 $ pretty $ "exposed-modules:"
-            , indent 6 $ docRpcModuleList
-            , indent 4 $ pretty $ "build-depends: simple-rpc"
-            , emptyDoc
-            ]
 
 generateCabalServer :: Config -> Text
 generateCabalServer Config{..} =
@@ -253,7 +234,7 @@ generateCabalServer Config{..} =
                               $ map pretty $ zipCommas
                               $ ["base", "simple-rpc", libraryName]
                         ]
-            , indent 4 $ pretty [str|hs-source-dirs: #{serverDirectoryRelativeToCabal}|]
+            , indent 4 $ pretty [str|hs-source-dirs: #{serverDirectory}|]
             , indent 4 $ pretty "default-language: Haskell2010"
             , emptyDoc
             ]
@@ -264,8 +245,7 @@ generateCabalServer Config{..} =
 
 data SrcLoc =
     SrcLoc
-        { slRpcClient :: (Int, Int)
-        , slRpcServer :: (Int, Int)
+        { slRpcServer :: (Int, Int)
         }
     deriving (Show)
 
@@ -282,28 +262,20 @@ getAttributeSrcLoc inp =
             (maybe (Left $ val ++ " not found") Right)
             (Fold.findIndex ((==) (T.pack val) . T.take (length val)))
     srclocFold =
-        Fold.teeWith (\a b -> SrcLoc <$> a <*> b)
-            (Fold.teeWith (\a b -> (,) <$> a <*> b)
-                (lineNumFold "-- <rpc-client>")
-                (lineNumFold "-- </rpc-client>"))
+        fmap SrcLoc <$>
             (Fold.teeWith (\a b -> (,) <$> a <*> b)
                 (lineNumFold "-- <rpc-server>")
                 (lineNumFold "-- </rpc-server>"))
-
-slice :: Int -> Int -> [a] -> [a]
-slice i j = take (j - i) . drop i
 
 getModifiedCabalFile :: Config -> Text -> Either String Text
 getModifiedCabalFile conf inp =
     case getAttributeSrcLoc inpLines of
         Left err -> Left err
-        Right (SrcLoc (a, b) (i, j)) -> Right $
+        Right (SrcLoc (a, b)) -> Right $
             T.unlines
                  $ take (a + 1) inpLines
-                 ++ [generateCabalClient conf]
-                 ++ slice b (i + 1) inpLines
                  ++ [generateCabalServer conf]
-                 ++ drop j inpLines
+                 ++ drop b inpLines
     where
     inpLines = T.lines inp
 
@@ -315,25 +287,11 @@ modifyCabalFile conf@Config{..} = do
         Left err -> error err
         Right newVal -> T.writeFile cabalFile newVal
 
-makeRpcModules :: Config -> IO ()
-makeRpcModules conf@Config{..} =
-    forM_ rpcModuleList
-        $ \rpcMod@(RpcModule{..}) -> do
-              let rpcModuleName = rpcModulePrefix ++ "." ++ moduleName
-                  libraryDirectory =
-                      takeDirectory cabalFile </> libraryRelativeToCabal
-                  dest = libraryDirectory </> dotToSlash rpcModuleName <.> ".hs"
-                  destDirectory = takeDirectory dest
-              Cmd.toStdout [str|mkdir -p #{destDirectory}|]
-              T.writeFile dest $ generateModule conf rpcMod
-
 makeRpcServer :: Config -> IO ()
 makeRpcServer conf@(Config{..}) = do
-    let serverDirectory =
-            takeDirectory cabalFile </> serverDirectoryRelativeToCabal
-        dest = serverDirectory </> "Main.hs"
+    let dest = serverDirectory </> "Main.hs"
     Cmd.toStdout [str|mkdir -p #{serverDirectory}|]
-    T.writeFile dest $ generateServer conf
+    T.writeFile dest =<< generateServer conf
 
 main :: IO ()
 main = do
@@ -344,6 +302,5 @@ main = do
             let value = fromMaybe (error "Improper JSON") mvalue
             let config = parseConfigFromValue value
             modifyCabalFile config
-            makeRpcModules config
             makeRpcServer config
-        _ -> error "1 argument is required"
+        _ -> error "Exactly one argument is accepted"
